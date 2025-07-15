ff1f6cc49b22d9589abbe35a1a26493637564cd4
Felipe Silva <150200576+Feliipe-Silva@users.noreply.github.com>
2025-07-14 22:28:35 -0300

    Create func_arrow_com_this_&_bind.js

---
+++ func_arrow_com_this_&_bind.js
@@ -0,0 +1,33 @@
+
+function pessoa() {
+    this.idade = Number(0);
+
+    const id_intervalo = setInterval(() => {
+        this.idade++;
+        console.log(this.idade);
+
+        if (this.idade === 5){
+            clearInterval(id_intervalo);
+        };
+    }, 1000)
+};
+
+new pessoa;
+
+// ----------------------------------------------
+
+
+
+function teste() {
+    const _this = this;     // aqui o this foi atribuído para não perder sua herança! 
+    _this.nome = 'João';    // variável com herança garantida.
+
+    function imperimir() {
+        console.log('Nome: ' + _this.nome);
+    };
+    // retorno correto, retorna o valor final do resultado da função. E não sua execução! 
+    return imperimir;  // retorna a função 'imprimir', e não o resultado de sua execução.
+};
+
+const atribui_teste = teste();
+atribui_teste();
